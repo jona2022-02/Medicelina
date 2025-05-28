f5f3cf6b7da6aab525b331dbd71b1ec85b1c9986
Jonathan Hernandez <166683841+jona2022-02@users.noreply.github.com>
2025-05-28 16:54:37 -0600

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBl8COPnwdflRd7E0GyxwBfQ3dIg4KD98g",
-  authDomain: "medic-5c272.firebaseapp.com",
-  projectId: "medic-5c272",
-  storageBucket: "medic-5c272.firebasestorage.app",
-  messagingSenderId: "309596331106",
-  appId: "1:309596331106:web:c31a481668df73c79d453b"
+  apiKey:",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "medi.firebasestorage.app",
+  messagingSenderId: "30",
+  "
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
